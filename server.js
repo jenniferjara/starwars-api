66cd44f5aafc1920208f99a238568f4cdb1cbf82
jenniferjara <lisbet.jara@gmail.com>
2016-11-13 23:07:34 -0500

    cambios para heroku

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 app.set("port", (process.env.PORT || 1500));
 app.use(express.static(__dirname + "/public"));
+app.get('/', function(req, res) {
+  res.render("index.html");
+});
 app.listen(app.get("port"), function(){
 	console.log("servidor encendido");
 });
