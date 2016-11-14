1ca9d9b24f411eb869f50b336a80211f5c663410
jenniferjara <lisbet.jara@gmail.com>
2016-11-14 11:39:57 -0500

    cambios en https

--- public/js/app.js
+++ public/js/app.js
@@ -50,7 +50,7 @@ var especies = function(res){
 	var spe = "";
 	$.each(res.results, function(i, espec){
 		var d = "";
-		var n = "http://swapi.co/api/people/";
+		var n = "https://swapi.co/api/people/";
 		$.each(espec.people, function(i, direc){
 			console.log(direc);
 			d += direc.replace(n,"");
@@ -70,7 +70,7 @@ var mostrarPersonaje = function(){
 	var numDirec = $(this).val().split("/");
 	/*console.log(numDirec);*/
 	for(var i =0, l = numDirec.length; i<l; i++){
-   		$.getJSON("http://swapi.co/api/people/" + numDirec[i]+"/", function(resp){
+   		$.getJSON("https://swapi.co/api/people/" + numDirec[i]+"/", function(resp){
    			var cadaEsp = template
    							.replace("{{name}}", resp.name)
    							.replace("{{url}}", resp.url);
@@ -79,8 +79,8 @@ var mostrarPersonaje = function(){
 	}
 }
 var iniciar = function(){
-	$.getJSON("http://swapi.co/api/people/", datos);
-	$.getJSON("http://swapi.co/api/species/", especies);
+	$.getJSON("https://swapi.co/api/people/", datos);
+	$.getJSON("https://swapi.co/api/species/", especies);
 	$("#next").click(siguiente);
 	$("#prev").click(siguiente);
 	$("#people").on("click", ".about", datosPersonaje);
